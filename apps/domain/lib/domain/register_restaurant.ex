493b1204866d2ce280c9da124ef337d00d865e4b
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-10 23:36:56 +0100

    Fix a warning

--- apps/domain/lib/domain/register_restaurant.ex
+++ apps/domain/lib/domain/register_restaurant.ex
@@ -1,5 +1,5 @@
 defmodule LunchRoulette.Business.RegisterRestaurant do
-  def register(restaurant, persistance) do
+  def register(restaurant, _persistance) do
     {:error, {:invalid_restaurant_name, restaurant}}
   end
 end
