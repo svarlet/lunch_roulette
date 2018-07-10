c861bdf50b31d808f5575ac67873c066b04b4217
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-10 22:04:47 +0100

    Prepare a list of failing tests

---
+++ apps/domain/test/domain/add_restaurant_test.exs
@@ -0,0 +1,13 @@
+defmodule LunchRoulette.Business.AddRestaurantTest do
+  use ExUnit.Case, async: true
+
+  test "restaurant exists"
+
+  test "restaurant does not exist"
+
+  test "nil"
+
+  test "empty name"
+
+
+end
