e65b1b6756a072493b4a2109eb0e42e88de53db5
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-10 20:01:08 +0100

    Delete autogenerated crap

--- apps/domain/lib/domain.ex
+++ apps/domain/lib/domain.ex
@@ -1,18 +1,2 @@
 defmodule Domain do
-  @moduledoc """
-  Documentation for Domain.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Domain.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
