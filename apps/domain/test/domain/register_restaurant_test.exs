7560bdfe6a56257485402cbe30c1305d46b8fb9a
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-10 23:47:37 +0100

    Prevent saving the restaurant and report the error when name is ""

--- apps/domain/test/domain/register_restaurant_test.exs
+++ apps/domain/test/domain/register_restaurant_test.exs
@@ -43,4 +43,20 @@ defmodule LunchRoulette.Business.RegisterRestaurantWithEmptyNameTest do
     assert {:error, {:invalid_restaurant_name, ""}} ==
              RegisterRestaurant.register("", persistance, presenter)
   end
+
+  test "does not attempt to save the restaurant" do
+    persistance = fn _ -> send(self(), :attempted_save) end
+    presenter = fn _ -> :ok end
+
+    RegisterRestaurant.register("", persistance, presenter)
+
+    refute_received(:attempted_save)
+  end
+
+  test "reports the error via the presenter" do
+    persistance = fn _ -> :ok end
+    presenter = fn error -> send(self(), error) end
+    RegisterRestaurant.register("", persistance, presenter)
+    assert_received({:error, {:invalid_restaurant_name, ""}})
+  end
 end
