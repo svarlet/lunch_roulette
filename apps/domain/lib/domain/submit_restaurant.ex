e623cd4807dfa20690fceb2c9cec9e637585bc5a
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-13 19:04:21 +0100

    Extract a registered? function

--- apps/domain/lib/domain/submit_restaurant.ex
+++ apps/domain/lib/domain/submit_restaurant.ex
@@ -1,9 +1,13 @@
 defmodule LunchRoulette.Business.SubmitRestaurant do
   def process(restaurant, data_store) do
-    if Enum.member?(data_store, restaurant) do
+    if registered?(data_store, restaurant) do
       data_store
     else
       [restaurant | data_store]
     end
+  end
+
+  defp registered?(data_store, restaurant) do
+    Enum.member?(data_store, restaurant)
   end
 end
