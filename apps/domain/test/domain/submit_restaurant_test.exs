658ed28b5ae75aa52ffc69e52d30ab83d0f055d9
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-22 19:08:01 +0100

    Extract shared test config to module attribute

--- apps/domain/test/domain/submit_restaurant_test.exs
+++ apps/domain/test/domain/submit_restaurant_test.exs
@@ -15,35 +15,29 @@ defmodule LunchRoulette.Business.SubmitRestaurantTest do
 
   defmock(Feedback.Mock, for: Feedback)
 
+  @config %Config{
+    validator_mod: Validator.Mock,
+    shortlist_mod: Shortlist.Mock,
+    feedback_mod: Feedback.Mock
+  }
+
   test "restaurant not listed" do
     restaurant = %Restaurant{name: Faker.Company.name()}
-
-    config = %Config{
-      validator_mod: Validator.Mock,
-      shortlist_mod: Shortlist.Mock,
-      feedback_mod: Feedback.Mock
-    }
 
     expect(Validator.Mock, :validate, fn ^restaurant -> {:ok, restaurant} end)
     expect(Shortlist.Mock, :shortlist, fn ^restaurant -> {:ok, restaurant} end)
     expect(Feedback.Mock, :report_success, fn ^restaurant -> :ok end)
 
-    SubmitRestaurant.submit(restaurant, config)
+    SubmitRestaurant.submit(restaurant, @config)
   end
 
   test "restaurant already shortlisted" do
     restaurant = %Restaurant{name: Faker.Company.name()}
 
-    config = %Config{
-      validator_mod: Validator.Mock,
-      shortlist_mod: Shortlist.Mock,
-      feedback_mod: Feedback.Mock
-    }
-
     expect(Validator.Mock, :validate, fn ^restaurant -> {:ok, restaurant} end)
     expect(Shortlist.Mock, :shortlist, fn ^restaurant -> {:error, :already_shortlisted} end)
     expect(Feedback.Mock, :report_already_shortlisted, fn ^restaurant -> :ok end)
 
-    SubmitRestaurant.submit(restaurant, config)
+    SubmitRestaurant.submit(restaurant, @config)
   end
 end
