eeef9b87ab3c776a9879ee1a31a87885257f4985
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-22 20:48:40 +0100

    Name the "logic" module Interactor

--- apps/domain/lib/domain/submit_restaurant.ex
+++ apps/domain/lib/domain/submit_restaurant.ex
@@ -26,22 +26,24 @@ defmodule LunchRoulette.Business.SubmitRestaurant do
     defstruct [:validator_mod, :shortlist_mod, :feedback_mod]
   end
 
-  def submit(restaurant, config) do
-    with {:ok, ^restaurant} <- config.validator_mod.validate(restaurant),
-         {:ok, ^restaurant} <- config.shortlist_mod.shortlist(restaurant) do
-      config.feedback_mod.report_success(restaurant)
-    else
-      {:error, :already_shortlisted} ->
-        config.feedback_mod.report_already_shortlisted(restaurant)
+  defmodule Interactor do
+    def submit(restaurant, config) do
+      with {:ok, ^restaurant} <- config.validator_mod.validate(restaurant),
+           {:ok, ^restaurant} <- config.shortlist_mod.shortlist(restaurant) do
+        config.feedback_mod.report_success(restaurant)
+      else
+        {:error, :already_shortlisted} ->
+          config.feedback_mod.report_already_shortlisted(restaurant)
 
-      {:error, :nil_submission} ->
-        config.feedback_mod.report_invalid_submission(:nil_submission)
+        {:error, :nil_submission} ->
+          config.feedback_mod.report_invalid_submission(:nil_submission)
 
-      {:error, :nil_restaurant_name} ->
-        config.feedback_mod.report_invalid_submission(:nil_restaurant_name)
+        {:error, :nil_restaurant_name} ->
+          config.feedback_mod.report_invalid_submission(:nil_restaurant_name)
 
-      {:error, :empty_restaurant_name} ->
-        config.feedback_mod.report_invalid_submission(:empty_restaurant_name)
+        {:error, :empty_restaurant_name} ->
+          config.feedback_mod.report_invalid_submission(:empty_restaurant_name)
+      end
     end
   end
 end
--- apps/domain/test/domain/submit_restaurant_test.exs
+++ apps/domain/test/domain/submit_restaurant_test.exs
@@ -1,11 +1,10 @@
-defmodule LunchRoulette.Business.SubmitRestaurantTest do
+defmodule LunchRoulette.Business.SubmitRestaurant.InteractorTest do
   use ExUnit.Case, async: true
 
   import Mox
 
   alias LunchRoulette.Business.Restaurant
-  alias LunchRoulette.Business.SubmitRestaurant
-  alias LunchRoulette.Business.SubmitRestaurant.{Validator, Shortlist, Feedback, Config}
+  alias LunchRoulette.Business.SubmitRestaurant.{Interactor, Validator, Shortlist, Feedback, Config}
 
   setup :verify_on_exit!
 
@@ -30,7 +29,7 @@ defmodule LunchRoulette.Business.SubmitRestaurantTest do
     expect(Shortlist.Mock, :shortlist, fn ^irrelevant_restaurant -> {:ok, irrelevant_restaurant} end)
     expect(Feedback.Mock, :report_success, fn ^irrelevant_restaurant -> :ok end)
 
-    SubmitRestaurant.submit(irrelevant_restaurant, @config)
+    Interactor.submit(irrelevant_restaurant, @config)
   end
 
   test "restaurant already shortlisted", %{irrelevant_restaurant: irrelevant_restaurant} do
@@ -38,27 +37,27 @@ defmodule LunchRoulette.Business.SubmitRestaurantTest do
     expect(Shortlist.Mock, :shortlist, fn ^irrelevant_restaurant -> {:error, :already_shortlisted} end)
     expect(Feedback.Mock, :report_already_shortlisted, fn ^irrelevant_restaurant -> :ok end)
 
-    SubmitRestaurant.submit(irrelevant_restaurant, @config)
+    Interactor.submit(irrelevant_restaurant, @config)
   end
 
   test "nil submission", %{irrelevant_restaurant: irrelevant_restaurant} do
     expect(Validator.Mock, :validate, fn ^irrelevant_restaurant -> {:error, :nil_submission} end)
     expect(Feedback.Mock, :report_invalid_submission, fn :nil_submission -> :ok end)
 
-    SubmitRestaurant.submit(irrelevant_restaurant, @config)
+    Interactor.submit(irrelevant_restaurant, @config)
   end
 
   test "nil restaurant name", %{irrelevant_restaurant: irrelevant_restaurant} do
     expect(Validator.Mock, :validate, fn ^irrelevant_restaurant -> {:error, :nil_restaurant_name} end)
     expect(Feedback.Mock, :report_invalid_submission, fn :nil_restaurant_name -> :ok end)
 
-    SubmitRestaurant.submit(irrelevant_restaurant, @config)
+    Interactor.submit(irrelevant_restaurant, @config)
   end
 
   test "empty restaurant name", %{irrelevant_restaurant: irrelevant_restaurant} do
     expect(Validator.Mock, :validate, fn ^irrelevant_restaurant -> {:error, :empty_restaurant_name} end)
     expect(Feedback.Mock, :report_invalid_submission, fn :empty_restaurant_name -> :ok end)
 
-    SubmitRestaurant.submit(irrelevant_restaurant, @config)
+    Interactor.submit(irrelevant_restaurant, @config)
   end
 end
