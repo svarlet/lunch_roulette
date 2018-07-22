268730828a0dbe21accff96fd7aa4cc87f90c0a1
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-22 23:17:02 +0100

    SubmissionValidator rejects restaurants with empty name

--- apps/domain/lib/domain/submit_restaurant/submission_validator.ex
+++ apps/domain/lib/domain/submit_restaurant/submission_validator.ex
@@ -1,7 +1,13 @@
 defmodule LunchRoulette.Business.SubmitRestaurant.Validator.SubmissionValidator do
   @behaviour LunchRoulette.Business.SubmitRestaurant.Validator
 
+  alias LunchRoulette.Business.Restaurant
+
   def validate(nil) do
     {:error, :nil_submission}
+  end
+
+  def validate(%Restaurant{name: ""}) do
+    {:error, :empty_restaurant_name}
   end
 end
--- apps/domain/test/domain/submit_restaurant/validator_test.exs
+++ apps/domain/test/domain/submit_restaurant/validator_test.exs
@@ -1,9 +1,14 @@
 defmodule LunchRoulette.Business.SubmitRestaurant.ValidatorTest do
   use ExUnit.Case, async: true
 
+  alias LunchRoulette.Business.Restaurant
   alias LunchRoulette.Business.SubmitRestaurant.Validator.SubmissionValidator
 
   test "nil restaurant" do
     assert {:error, :nil_submission} == SubmissionValidator.validate(nil)
+  end
+
+  test "empty name" do
+    assert {:error, :empty_restaurant_name} == SubmissionValidator.validate(%Restaurant{name: ""})
   end
 end
