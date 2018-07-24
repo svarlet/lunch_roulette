ec610571620f3fb18694efd7243552019ec00495
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-24 23:43:32 +0100

    Extract a helper

--- apps/domain/test/domain/submit_restaurant/agent_shortlist_test.exs
+++ apps/domain/test/domain/submit_restaurant/agent_shortlist_test.exs
@@ -15,14 +15,16 @@ defmodule Domain.SubmitRestaurant.AgentShortlistTest do
   test "put_in 3 restaurants" do
     {:ok, pid} = Agent.start_link(fn -> MapSet.new() end)
     shortlist = %AgentShortlist{pid: pid}
-
-    restaurants =
-      1..3
-      |> Enum.map(fn unique_name -> "restaurant_#{unique_name}" end)
-      |> Enum.map(fn name -> %Restaurant{name: name} end)
+    restaurants = create_few_restaurants()
 
     Enum.each(restaurants, &Shortlist.put_in(shortlist, &1))
 
     assert MapSet.new(restaurants) == Agent.get(shortlist.pid, &(&1))
+  end
+
+  defp create_few_restaurants() do
+    1..3
+    |> Enum.map(fn unique_name -> "restaurant_#{unique_name}" end)
+    |> Enum.map(fn name -> %Restaurant{name: name} end)
   end
 end
