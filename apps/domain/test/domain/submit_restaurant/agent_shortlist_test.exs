b12cb6809657974dbc834d8b0fbb83e34dbb4ef2
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-24 23:39:59 +0100

    AgentShortlist accepts many restaurants

--- apps/domain/test/domain/submit_restaurant/agent_shortlist_test.exs
+++ apps/domain/test/domain/submit_restaurant/agent_shortlist_test.exs
@@ -11,4 +11,18 @@ defmodule Domain.SubmitRestaurant.AgentShortlistTest do
     assert {:ok, shortlist} == Shortlist.put_in(shortlist, restaurant)
     assert Agent.get(shortlist.pid, fn mapset -> MapSet.member?(mapset, restaurant) end)
   end
+
+  test "put_in 3 restaurants" do
+    {:ok, pid} = Agent.start_link(fn -> MapSet.new() end)
+    shortlist = %AgentShortlist{pid: pid}
+
+    restaurants =
+      1..3
+      |> Enum.map(fn unique_name -> "restaurant_#{unique_name}" end)
+      |> Enum.map(fn name -> %Restaurant{name: name} end)
+
+    Enum.each(restaurants, &Shortlist.put_in(shortlist, &1))
+
+    assert MapSet.new(restaurants) == Agent.get(shortlist.pid, &(&1))
+  end
 end
