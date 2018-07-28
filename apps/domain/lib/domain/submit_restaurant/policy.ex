f705dc451872f6ae71512ae29dcc31d7c7a4b4b1
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-28 22:30:24 +0100

    Introduce a save function dependency

--- apps/domain/lib/domain/submit_restaurant/policy.ex
+++ apps/domain/lib/domain/submit_restaurant/policy.ex
@@ -1,11 +1,11 @@
 defmodule Domain.SubmitRestaurant.Policy do
   alias Domain.Restaurant
 
-  def submit(nil) do
+  def submit(nil, _save) do
     {:error, {:validation, :no_restaurant}}
   end
 
-  def submit(%Restaurant{name: name}) when name in ["", nil] do
+  def submit(%Restaurant{name: name}, _save) when name in ["", nil] do
     {:error, {:validation, :anonymous}}
   end
 end
--- apps/domain/test/domain/submit_restaurant/submit_restaurant_test.exs
+++ apps/domain/test/domain/submit_restaurant/submit_restaurant_test.exs
@@ -5,17 +5,19 @@ defmodule Domain.SubmitRestaurantTest do
 
   alias Domain.Restaurant
 
+  defp unexpected_save(_), do: raise :unexpected_save
+
   test "rejects a nil restaurant" do
-    assert {:error, {:validation, :no_restaurant}} == submit(nil)
+    assert {:error, {:validation, :no_restaurant}} == submit(nil, &unexpected_save/1)
   end
 
   test "reject a restaurant with no name" do
     anonymous_restaurant = %Restaurant{name: nil}
-    assert {:error, {:validation, :anonymous}} == submit(anonymous_restaurant)
+    assert {:error, {:validation, :anonymous}} == submit(anonymous_restaurant, &unexpected_save/1)
   end
 
   test "reject a restaurant with an empty name" do
     anonymous_restaurant = %Restaurant{name: ""}
-    assert {:error, {:validation, :anonymous}} == submit(anonymous_restaurant)
+    assert {:error, {:validation, :anonymous}} == submit(anonymous_restaurant, &unexpected_save/1)
   end
 end
