0b924c1f370553fac2169e1d942c0df2da286c7e
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-29 01:44:08 +0100

    Add some typespecs

--- apps/domain/lib/domain/submit_restaurant/policy.ex
+++ apps/domain/lib/domain/submit_restaurant/policy.ex
@@ -2,6 +2,9 @@ defmodule Domain.SubmitRestaurant.Policy do
   alias Domain.SubmitRestaurant.{Canonicalization, Validation}
   alias Result
 
+  @type restaurant_saver :: (Restaurant.t -> Result.t)
+
+  @spec submit(Restaurant.t, restaurant_saver) :: Result.t
   def submit(restaurant, save) do
     restaurant
     |> Canonicalization.canonicalize()
