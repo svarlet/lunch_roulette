910c71d2dfbfa7beb67346a519bb0df1e0fed4fc
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-31 00:36:05 +0100

    Remove duplication in the test thanks to Faker

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -2,19 +2,13 @@ defmodule Web.SubmitRestaurantControllerTest do
   use Web.ConnCase, async: true
 
   describe "the submission of a valid not shortlisted restaurant" do
-    setup %{conn: conn, restaurant_name: name} do
-      params = %{"name" => name}
+    setup %{conn: conn} do
+      params = %{"name" => "The #{Faker.Color.En.fancy_name} restaurant"}
       path = submit_restaurant_path(conn, :submit)
-      %{conn: post(conn, path, params), name: name}
+      %{conn: post(conn, path, params), name: params["name"]}
     end
 
-    @tag restaurant_name: "Not a shortlisted restaurant"
     test "displays a success feedback", %{conn: conn, name: name} do
-      assert html_response(conn, 200) =~ success_feedback_for(name)
-    end
-
-    @tag restaurant_name: "Yet another not shortlisted restaurant"
-    test "displays a success feedback for any name", %{conn: conn, name: name} do
       assert html_response(conn, 200) =~ success_feedback_for(name)
     end
 
