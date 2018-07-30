edcf42825d855d51ac2c2ebf2f9c3eca1df4214c
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-30 19:21:15 +0100

    Improve the structure and names of the test

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -1,13 +1,13 @@
 defmodule Web.SubmitRestaurantControllerTest do
   use ExUnit.Case, async: true
 
-  describe "valid not shortlisted restaurant" do
-    test "set http status to 200" do
-      irrelevant_params = %{}
+  describe "the submission of a valid not shortlisted restaurant" do
+    test "responds with http status 200" do
+      params = %{"name" => "Not a shortlisted restaurant"}
 
       conn =
         %Plug.Conn{}
-        |> Web.SubmitRestaurantController.submit(irrelevant_params)
+        |> Web.SubmitRestaurantController.submit(params)
 
       assert %Plug.Conn{status: 200} = conn
     end
