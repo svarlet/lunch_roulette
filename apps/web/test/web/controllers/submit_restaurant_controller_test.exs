36d882ec7e622128a32e89464e5173bc0284a02e
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-31 00:01:31 +0100

    Remove obsolete code

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -1,8 +1,6 @@
 defmodule Web.SubmitRestaurantControllerTest do
   use Web.ConnCase, async: true
 
-  alias Plug.Conn
-
   describe "the submission of a valid not shortlisted restaurant" do
     setup %{conn: conn} do
       params = %{"name" => "Not a shortlisted restaurant"}
@@ -10,16 +8,9 @@ defmodule Web.SubmitRestaurantControllerTest do
       %{conn: post(conn, path, params), params: params}
     end
 
-    test "responds with http status 200", %{conn: conn} do
-      assert %Conn{status: 200} = conn
-    end
-
-    test "responds with content-type: text/html and utf-8 charset", %{conn: conn} do
-      assert response_content_type(conn, :html) =~ "charset=utf-8"
-    end
-
     test "displays a success feedback", %{conn: conn, params: params} do
-      assert html_response(conn, 200) =~ "Thanks! you've successfully shortlisted #{params["name"]}."
+      assert html_response(conn, 200) =~
+               "Thanks! you've successfully shortlisted #{params["name"]}."
     end
   end
 end
