b2b559fa16f31036add0b352a3ccae1418e75f1f
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-30 23:57:25 +0100

    Provide feedback on the successful addition to the shortlist of Not a successful restaurant

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -7,7 +7,7 @@ defmodule Web.SubmitRestaurantControllerTest do
     setup %{conn: conn} do
       params = %{"name" => "Not a shortlisted restaurant"}
       path = submit_restaurant_path(conn, :submit)
-      %{conn: post(conn, path, params)}
+      %{conn: post(conn, path, params), params: params}
     end
 
     test "responds with http status 200", %{conn: conn} do
@@ -16,6 +16,10 @@ defmodule Web.SubmitRestaurantControllerTest do
 
     test "responds with content-type: text/html and utf-8 charset", %{conn: conn} do
       assert response_content_type(conn, :html) =~ "charset=utf-8"
+    end
+
+    test "displays a success feedback", %{conn: conn, params: params} do
+      assert html_response(conn, 200) =~ "Thanks! you've successfully shortlisted #{params["name"]}."
     end
   end
 end
