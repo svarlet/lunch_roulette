9a5c4a9f2cd0e70e4dd53a9632651b6e7ec764a9
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-31 00:06:10 +0100

    Parameterize the test with a restaurant_name tag

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -2,15 +2,16 @@ defmodule Web.SubmitRestaurantControllerTest do
   use Web.ConnCase, async: true
 
   describe "the submission of a valid not shortlisted restaurant" do
-    setup %{conn: conn} do
-      params = %{"name" => "Not a shortlisted restaurant"}
+    setup %{conn: conn, restaurant_name: name} do
+      params = %{"name" => name}
       path = submit_restaurant_path(conn, :submit)
-      %{conn: post(conn, path, params), params: params}
+      %{conn: post(conn, path, params), name: name}
     end
 
-    test "displays a success feedback", %{conn: conn, params: params} do
+    @tag restaurant_name: "Not a shortlisted restaurant"
+    test "displays a success feedback", %{conn: conn, name: name} do
       assert html_response(conn, 200) =~
-               "Thanks! you've successfully shortlisted #{params["name"]}."
+               "Thanks! you've successfully shortlisted #{name}."
     end
   end
 end
