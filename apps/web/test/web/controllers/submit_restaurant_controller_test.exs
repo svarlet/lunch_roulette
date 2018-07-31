6611d143122cea4187f79e0a55d18463f5078ef5
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-31 23:21:35 +0100

    Extract a setup function

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -53,7 +53,7 @@ defmodule Web.SubmitRestaurantControllerTest do
   end
 
   describe "the submission of an invalid restaurant" do
-    test "redirects to the home page", %{conn: conn} do
+    setup %{conn: conn} do
       params = %{"name" => Faker.Company.name()}
       path = submit_restaurant_path(conn, :submit)
 
@@ -67,24 +67,15 @@ defmodule Web.SubmitRestaurantControllerTest do
         |> assign(:di_container, di_container)
         |> post(path, params)
 
+      %{conn: conn, name: params["name"]}
+    end
+
+    test "redirects to the home page", %{conn: conn} do
       assert redirected_to(conn) == page_path(conn, :home)
     end
 
-    test "report error with a flash message", %{conn: conn} do
-      params = %{"name" => Faker.Company.name()}
-      path = submit_restaurant_path(conn, :submit)
-
-      di_container = %{
-        save: fn _ -> {:error, {:irrelevant_category, :irrelevant_reason}} end
-      }
-
-      conn =
-        conn
-        |> recycle()
-        |> assign(:di_container, di_container)
-        |> post(path, params)
-
-      assert %{"error" => "Error: irrelevant_category of #{params["name"]} failed because of irrelevant_reason."} == get_flash(conn)
+    test "report error with a flash message", %{conn: conn, name: name} do
+      assert %{"error" => "Error: irrelevant_category of #{name} failed because of irrelevant_reason."} == get_flash(conn)
     end
   end
 end
