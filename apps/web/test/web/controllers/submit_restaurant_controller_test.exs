88604f02ad5a0da0470d7ca797e7dc9981106dd7
Sebastien Varlet <sebastien.varlet@gmail.com>
2018-07-31 08:30:44 +0100

    Reformat code

--- apps/web/test/web/controllers/submit_restaurant_controller_test.exs
+++ apps/web/test/web/controllers/submit_restaurant_controller_test.exs
@@ -3,7 +3,7 @@ defmodule Web.SubmitRestaurantControllerTest do
 
   describe "the submission of a valid not shortlisted restaurant" do
     setup %{conn: conn} do
-      params = %{"name" => "The #{Faker.Color.En.fancy_name} restaurant"}
+      params = %{"name" => "The #{Faker.Color.En.fancy_name()} restaurant"}
       path = submit_restaurant_path(conn, :submit)
       %{conn: post(conn, path, params), name: params["name"]}
     end
